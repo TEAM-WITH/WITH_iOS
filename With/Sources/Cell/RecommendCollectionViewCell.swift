13a6f6fcb064f53fb0ce78dd3d5744d1f195ae9e
june kwon <junnegeem@gmail.com>
2019-12-24 21:58:00 +0900

    [CREATE] Recommend CollectionView 생성

---
+++ With/Sources/Cell/RecommendCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecommendCollectionViewCell.swift
+//  With
+//
+//  Created by JUNE on 2019/12/24.
+//  Copyright © 2019 ns. All rights reserved.
+//
+
+import UIKit
+
+class RecommendCollectionViewCell: UICollectionViewCell {
+    
+}
