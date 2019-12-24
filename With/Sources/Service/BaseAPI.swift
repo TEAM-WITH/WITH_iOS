0e3e74d0be66745ef14c2792a0a2459c63cd49a3
namsoo5 <are34@naver.com>
2019-12-24 16:25:16 +0900

    [CREATE] API구조체 생성
    URL관리를위해 구조체 생성

---
+++ With/Sources/Service/BaseAPI.swift
@@ -0,0 +1,15 @@
+//
+//  BaseAPI.swift
+//  With
+//
+//  Created by 남수김 on 2019/12/24.
+//  Copyright © 2019 ns. All rights reserved.
+//
+
+import Foundation
+
+struct BaseAPI {
+    static let baseURL = ""
+    static let socketURL = ""
+    
+}
