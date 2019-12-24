a4c627b8e7de177c1edd66a83f700e37041e43b3
june kwon <junnegeem@gmail.com>
2019-12-25 02:30:11 +0900

    [CREATE] Recent CollectionView 생성 및 구현
    CollectionView에 대한  update
    CollectionView가 세개가 되었기 때문에 이에따른 함수 업데이트
    늘어나는 view와 label에 대한 autolayout

---
+++ With/Sources/Cell/RecentCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecentCollectionViewCell.swift
+//  With
+//
+//  Created by JUNE on 2019/12/25.
+//  Copyright © 2019 ns. All rights reserved.
+//
+
+import UIKit
+
+class RecentCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var recentWriteLabel: UILabel!
+    @IBOutlet weak var recentProImage: UIImageView!
+    @IBOutlet weak var recentNameLabel: UILabel!
+    @IBOutlet weak var recentCountryboxColor: UIView!
+    @IBOutlet weak var recentCountryLabel: UILabel!
+    
+}
--- With/Sources/ViewController/HomeViewController.swift
+++ With/Sources/ViewController/HomeViewController.swift
@@ -12,6 +12,7 @@ class HomeViewController: UIViewController {
 //     var mateArray = []
     @IBOutlet weak var mateCollectionView: UICollectionView!
     @IBOutlet weak var recommendCollectionView: UICollectionView!
+    @IBOutlet weak var recentCollectonView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         setCollection()
@@ -23,6 +24,7 @@ extension HomeViewController {
         // mate, recommend datasource
         self.mateCollectionView.dataSource = self
         self.recommendCollectionView.dataSource = self
+        self.recentCollectonView.dataSource = self
     }
 }
 // 개수에 관한 collection
@@ -35,9 +37,14 @@ extension HomeViewController: UICollectionViewDataSource {
 //                count = 6
 //            }
 //            return count
+            return 6
         }else if collectionView == recommendCollectionView {
+            return 6
         }
-        return 6
+        else if collectionView == recentCollectonView {
+            return 6
+        }
+        return 0
     }
     // 다중 collection view
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -47,7 +54,10 @@ extension HomeViewController: UICollectionViewDataSource {
         }else if collectionView == recommendCollectionView{
        let  cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecommendCell", for: indexPath) as! RecommendCollectionViewCell
             return cell
-        }
+        }else if collectionView == recentCollectonView{
+        let  cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecentCell", for: indexPath) as! RecentCollectionViewCell
+             return cell
+         }
         return UICollectionViewCell()
     }
 }
