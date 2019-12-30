64ce6f4fbac35558f2b2f837c6ef0815aab134d3
namsoo5 <are34@naver.com>
2019-12-30 14:39:02 +0900

    [UPDATE] 회원가입 통신구현

---
+++ With/Sources/ViewController/ServiceTestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ServiceTestViewController.swift
+//  With
+//
+//  Created by 남수김 on 2019/12/29.
+//  Copyright © 2019 ns. All rights reserved.
+//
+
+import UIKit
+
+class ServiceTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
